074c5ed0ac751994b6a81c0a84fa69a5fab5f3d4
erik-frontend <130284367+erik-frontend@users.noreply.github.com>
2025-09-26 20:23:48 +0200

    commit

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -1,9 +1,60 @@
+import { getUnsplashImage } from '@/lib/unsplash'
+import { truncateText } from '@/lib/utils'
+import Image from 'next/image'
+import Link from 'next/link'
+import { notFound } from 'next/navigation'
 import React from 'react'
 
-const SinglePage = () => {
+ async function getPost(id) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    next: {revalidate: 3600}
+  })  
+
+  if(res.status === 404) return null
+  if(!res.ok) throw new Error("Faild to fetch post")
+    return res.json()
+}
+
+async function getUser(id) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+    next: {revalidate: 3600}
+  })
+
+  if(!res.ok) return null
+  return res.json()
+}
+
+async function SinglePage({params}) {
+
+  // console.log(params);
+  
+  const {slug} = await params
+
+  const post = await getPost(slug)
+
+  const user = await getUser(slug)
+
+  console.log(user);
+  
+
+  if(!post) return notFound()
+
+  if(!user) return "Unknown author"
+
+  const data = "04.09.2025"
+
+  const [imageUrl] = await Promise.all([
+    getUnsplashImage(post.title, {size: "regular"}),
+    getUser(user.id)
+  ])
+
   return (
-    <div>
-        <h1>Single page</h1>
+    <div className='mx-auto max-w-[128rem] py-20 px-20'>
+      <div className="grid gap-16 xl:grid-cols-2 items-start">
+        <div className="relative w-full h-[420px] overflow-hidden rounded-2xl">
+          <Image src={imageUrl ?? "/file.svg"} alt={post.title} fill className='object-cover' sizes='(min-width: 1280px) 50vw, 100vw' priority/>
+        </div>
+      </div>
     </div>
   )
 }
--- components/postCard/PostCard.jsx
+++ components/postCard/PostCard.jsx
@@ -1,30 +1,15 @@
+import { getUnsplashImage } from '@/lib/unsplash'
+import { truncateText } from '@/lib/utils'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
-
-async function getUnsplashImage(query, index, size="small") {
-    const key = process.env.UNSPLASH_ACCESS_KEY
-    if(!key) return null
-}
-
-try {
-    const res = await fetch(`https://api.unsplash.com/search/photos?query=${encodeURIComponent(query)}&per_page=10`,
-    {
-        headers: {Autorization: `Client-ID ${key}`},
-        next: {revalidate: 86400}
-    })
-
-    if(!res.ok) return null
-
-    const data = await res.json()
-    const result = Array.isArray(data?.result) ? data.result : []
-    const img = result.length ? result[index % result.length] : null
-    return img?.urls?.[size] ?? null
-} catch (error) {
-    
-}
 
 const PostCard = async ({ post, index = 0 }) => {
 
     const imageUrl = (await getUnsplashImage(post.title, index)) ?? "/file.svg"
+
+    // console.log(imageUrl);
+    
 
     const data = post.data ?? "01.09.2025"
 
@@ -34,14 +19,14 @@ const PostCard = async ({ post, index = 0 }) => {
                 <div className="relative w-[90%] h-[400px] overflow-hidden rounded-xl">
                     <Image src={imageUrl} alt='' fill className='object-cover' sizes='(mix-width: 1280px) 50vw, 100vw' priority={index < 2} />
                 </div>
-                <time>{data}</time>
+                <time className='block mb-4'>{data}</time>
             </div>
             <div className="">
-                <h3>
-                    {post.title}
+                <h3 className='text-3xl mb-4 capitalize'>
+                    {truncateText(post.title, 50)}
                 </h3>
-                <p>{post.body}</p>
-                <Link href="">Read more</Link>
+                <p className='text-xl mb-4'>{truncateText(post.body, 100)}</p>
+                <Link href={`/blog/${post.id}`} className='block border-2 px-4 py-3 w-[180px] text-center'>Read more</Link>
             </div>
         </article>
     )
